d043d80b66d3c6745a22786c5d79c732ef71c490
Paul <paulereverend@gmail.com>
2022-03-04 09:02:03 +0100

    ajout des reducs dans un json

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-console.log("hello world");
-
-var tauxReduc = [
-    { taux: 1000, reduc: 3 },
-    { taux: 5000, reduc: 5 },
-    { taux: 7000, reduc: 7 },
-    { taux: 10000, reduc: 10 }
-]
+var tauxReduc = require('./reduc.json');
 tauxReduc.forEach(tr => {
     console.log("Taux : " + tr.taux + "€ => " + tr.reduc + "%");
 });
