b224446f20d625797dbaa0f9d6700c9a50159e54
Hugo <hugo.huet.14@gmail.com>
2022-03-04 10:26:20 +0100

    calcul TTC et TVA

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 var tauxReduc = require('./reduc.json');
+var tva = require('./tva.json');
 
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
+console.log('Taux de réduction');
 tauxReduc.forEach(tr => {
     console.log("Taux : " + tr.taux + "€ => " + tr.reduc + "%");
+});
+console.log('Code TVA');
+tva.forEach(tva => {
+    console.log("Code : " + tva.code + " => " + tva.val + "%");
 });
 
 readline.question(`Quel est le prix unitaire du produit ? `, prix => {
     //if (!inputError(prix, "number")) {
     readline.question(`Quelle quantitié ? `, quantite => {
         //if (!inputError(quantite, "number")) {
-        console.log("Le prix total est de : " + prix * quantite + "€");
-        readline.close();
+        var prixTotalHT = prix * quantite
+        console.log("Prix total HT : " + prixTotalHT + "€");
+
+        readline.question(`Quel pourcentage de TVA souhaitez-vous appliquer ? (en %) `, tva => {
+            var prixTotalTTC = prixTotalHT * (tva / 100 + 1)
+            console.log('Prix total TTC : ' + prixTotalTTC + " €");
+
+            readline.close();
+        });
         //}
     });
     //}
